163bf2e0cff782dbf5faac6cd6843df8e7d8832d
Ethan Tam <etam4225@bths.edu>
2017-09-26 23:39:21 -0400

    edited student

--- Tam/src/attendance/Student.java
+++ Tam/src/attendance/Student.java
@@ -4,42 +4,60 @@ public class Student implements Attendee {
 
 	private String first;
 	private String last;
-	private static String studentPresent;
+	private boolean studentPresent = false;
 	public Student (String firstName, String lastName) {
 		this.first = firstName;
 		this.last = lastName;
 	}
 	public boolean isPresent() {
-		if(Student.studentPresent == "present")
+		if(this.studentPresent == true)
+		{
+			return true;
+		}
+		return false;
+		
+	}
+
+	public void setPresent(boolean present) {
+		this.studentPresent = true;
+		 
+	}
+
+	public String getFirstName() {
+		return this.first;
+	}
+
+	public String getLastName() {
+		return this.last;
+	}
+
+	
+	public boolean mathces(String first, String last) {
+		if(this.getFirstName().compareTo(first) == 0 && this.getLastName().compareTo(last) == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean matches(String last) {
+		if(this.getLastName().compareTo(last) == 0)
 		{
 			return true;
 		}
 		return false;
 	}
 
-	public void setPresent(boolean present) {
-		 
-	}
-
-	public String getFirstName() {
-		return null;
-	}
-
-	public String getLastName() {
-		return null;
-	}
-
-	
-	public boolean mathces(String first, String last) {
-		return false;
-	}
-
-	public boolean matches(String last) {
-		return false;
-	}
-
 	public String getReportString() {
-		return null;
+		while(this.first.length() < 20)
+		{
+			this.first += "";
+		}
+		this.last = this.first + this.last;
+		while(this.last.length() < 40)
+		{
+			this.last += "";
+		}
+		return this.last;
 	}
 
 }
