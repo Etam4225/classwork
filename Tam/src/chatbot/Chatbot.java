8323f17c8f54693ad6b19d4c891ee8faca43979d
Ethan Tam <etam4225@bths.edu>
2017-09-28 15:33:48 -0400

    created chatbot stuff

---
+++ Tam/src/chatbot/Chatbot.java
@@ -0,0 +1,32 @@
+package chatbot;
+
+public class Chatbot {
+
+	private String username;
+	private boolean chatting;
+	private Topic ethan;
+	
+	public Chatbot() {
+		ethan = new ChatbotEthan();
+		username = "Unknown User";
+		chatting = true;
+	}
+
+	public void startChatting() {
+		ChatbotMain.print("Hi! I am an intelligent machine that can respond to your input. Tell me your name.");
+		username = ChatbotMain.getInput();
+		while(chatting) {
+			ChatbotMain.print("What would you like to talk about?");
+			String response = ChatbotMain.getInput();
+			if(ethan.isTriggered(response)) {
+				chatting = false;//exits the while loop
+				//IMPORTANT TO KNOW YOU GET GRADED FOR THIS
+				ethan.talk(response);
+				
+			}else {
+				ChatbotMain.print("I'm sorry. I don't understand. I never said I was perfect.");
+			}
+		}
+	}
+
+}
---
+++ Tam/src/chatbot/ChatbotEthan.java
@@ -0,0 +1,16 @@
+package chatbot;
+
+public class ChatbotEthan implements Topic {
+
+	public ChatbotEthan() {
+	}
+
+	public void talk(String response) {
+		
+	}
+
+	public boolean isTriggered(String response) {
+		return false;
+	}
+
+}
