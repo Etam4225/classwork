311f1d84c422f99c405b09c9ed691f677d65ec1b
Ethan Tam <etam4225@bths.edu>
2017-10-16 15:31:12 -0400

    Learned more about Arrays

--- Tam/src/arrays/ArraysMain.java
+++ Tam/src/arrays/ArraysMain.java
@@ -1,31 +1,127 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class ArraysMain {
 
-	private int[] testArray;
+	private String[] suits;
+	private String[] values;
 	
 	public ArraysMain() {
+		suits = new String[4];
+		suits[0] = "Clubs";
+		suits[1] = "Hearts";
+		suits[2] = "Diamonds";
+		suits[3] = "Spades";
+		values = new String[13];
+		for(int i = 0; i < values.length; i++) {
+			values[i] = ""+(i+1);
+		}
+		values[0]="Ace";
+		values[12] = "King";
+		values[11] = "Queen";
+		values[10] = "Jack";
+		printDeck();
+		//System.out.println(Arrays.toString(testArray));
+	}
+	private String[] printDeck() {
+		String[] deck = new String[52];
+		int index = 0;
+		/*for(int i = 0; i < suits.length; i++) {
+			for(int f = 0; f < values.length; f++) {
+				System.out.println(values[f] + " of " + suits[i]);
+			}
+		}*/
+		for(String suit: suits) {
+			for(String value: values) {
+				deck[index] = value + " of " + suit;
+				index++;
+			}
+		}
+		return deck;
+	}
+	private void shuffle(int[] arr) {
+		int myNum1 = 0;
+		for(int i = 0; i < arr.length; i++) {
+			myNum1 = (int) (Math.random() * arr.length);
+			swap(arr, i, myNum1);
+		}
+		
+	}
+	/**
+	 * Swaps elements at i and j
+	 * @param arr
+	 * @param i
+	 * @param j
+	 */
+	private void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	/**
+	 * Populate arr with numbers from 1 to arr.length, in order
+	 * @param testArray2
+	 */
+	private void populate1ToN(int[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = (i+1);
+		}
+	}
+
+	private void countOccurences(int[] arr, int start, int end) {
+		//why create an array with this length
+		int[] counter = new int[end - start + 1];
+		
+		for(int value: arr) {
+			//why value - start?
+			counter[value - start]++;
+		}
+		for(int i = 0; i < counter.length; i++) {
+			System.out.println("The value " + (i+start) + " was rolled "+ counter[i] + " times.");
+		}
+	}
+
+	/**
+	 * This method populates arr with results from rolling 2 dice
+	 * @param arr
+	 */
+	private void populate(int[] arr) {
+		//does nothing (incorrect)
+		/*for(int value: arr) {
+			arr[value] = diceRoll(2);
+		}*/
+		//correct way
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = diceRoll(3);
+		}
+		
+	}
+
+
+	public void arrayNotes() {
 		//two ways to construct an array
 		int[] firstWay = {0,1,2,3,4,5};
 		//this way will only work with the declaration.
 		//will not work:
 		//firstWay = {6,7,8,9,10};
-		
-		int[] secondWay = new int[5];
+			
+		String[] secondWay = new String[5];
 		//you can go on, creating values at each index
-		secondWay[0] = 1;
-		secondWay[1] = 10;
-		
+		//secondWay[0] = 1;
+		//secondWay[1] = 10;
+				
 		//TWO WAYS TO ITERATE THROUGH AN ARRAY
 		for(int i = 0; i < secondWay.length; i++) {
 			System.out.println("The #" + i + " element is " + secondWay[i]);
 		}
 		//For each int in secondWay
-		for(int value: secondWay) {
+		for(String value: secondWay) {
 			System.out.println("Element is " + value);
 		}
+		//NOTE: primitive arrays are auto populated with 0
+		//OBJECT arrays are not populated (null)
 	}
-
 	public static void main(String[] args) {
 		ArraysMain sample = new ArraysMain();
 		/*
