25852de7d1e8a75ea3f398161706b0ae6125b6a7
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2017-11-10 11:05:29 -0500

    added environment

---
+++ Tam/src/environment/Environment.java
@@ -0,0 +1,25 @@
+package environment;
+
+public class Environment {
+
+	private int plants;
+	private int growthRate;
+	
+	public Environment(int plants) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Environment farm = new Environment(10);
+		System.out.println(farm);
+		farm.growPlants();
+
+	}
+
+	private void growPlants() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
